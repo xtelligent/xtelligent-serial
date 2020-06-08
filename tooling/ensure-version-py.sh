e3db4183949e0b4e92dff874fdf17179e1a035fd
Chris Grimes <chris@xtelligent.io>
2020-06-07 21:12:37 -0700

    Manual update to tooling.

---
+++ tooling/ensure-version-py.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+MYDIR="$(cd "$(dirname "$0")" && pwd)"
+export BNUM="${BUILDNUMBER:-2112}"
+cp "$MYDIR/../config" "$MYDIR/../version.py"
+mkdir -p "$MYDIR/../xtelligent_serial"
+VERFILE="$MYDIR/../xtelligent_serial/version.py"
+cp "$MYDIR/../config" "$VERFILE"
+echo "BUILDNUMBER=$BNUM" >> "$VERFILE"
+echo "$VERFILE"
+test -f "$VERFILE"
