5fb76b212300fe70bb612ffb0457f624b0e6cb5a
Development Team <development@xtelligent.io>
2020-06-26 22:59:15 +0000

    Generated documentation.

--- xtelligent_serial/data_structs/struct_descriptor.py
+++ xtelligent_serial/data_structs/struct_descriptor.py
@@ -2,15 +2,25 @@ from collections import namedtuple
 from dataclasses import Field
 from typing import Type, Iterable, Any
 
-DescriptorInterface = namedtuple('DescriptorInterface', ['name', 'is_instance', 'get_fields'])
-''' Describes a family of classes/structs such as dataclass or attrs.
+DescriptorInterface2 = namedtuple('DescriptorInterface', ['name', 'is_instance', 'get_fields'])
 
-    name: Name of the family/API (dataclass, attrs)
-    is_instance: Returns True if an instance is from that family. Note that the API
-        may not depend on inheritance for membership.
-    get_fields: Returns the fields of a type in the family/API. The result is an Iterable
-        of dataclasses.Field instances.
-'''
+class DescriptorInterface:
+    '''Describes a family of classes/structs such as dataclass or attrs.'''
+    @property
+    def name(self) -> str:
+        '''Name of the family/API (dataclass, attrs)'''
+        raise NotImplementedError()
+
+    def is_instance(self, target: Any) -> bool:
+        '''Returns True if an instance is from that family. Note that the API
+        may not leverage inheritance for membership.'''
+        raise NotImplementedError()
+
+    def get_fields(self, t: Type) -> Iterable[Field]:
+        '''Returns the fields of a type in the family/API. The result is an Iterable
+        of dataclasses.Field instances.'''
+        raise NotImplementedError()
+
 
 def is_instance_callback(target: Any) -> bool:
     '''Returns true if the target is an instance of the structure family.'''
@@ -22,4 +32,4 @@ def get_fields_callback(wanted_type: Type) -> Iterable[Field]:
     raise NotImplementedError()
 
 def describe_struct_family(name, is_instance: is_instance_callback, get_fields: get_fields_callback) -> DescriptorInterface:
-    return DescriptorInterface(name, is_instance, get_fields)
+    return DescriptorInterface2(name, is_instance, get_fields)
